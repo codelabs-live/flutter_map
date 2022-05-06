4a824b3bf5d3d2f55e8a64586010759bd82546f6
Kamlesh Kumar <dev.slinfy@gmail.com>
2022-05-06 17:32:33 +0530

    Initial commit

---
+++ lib/nearby_hospitals.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
+
+class FoodieMap extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FoodieMapState();
+  }
+}
+
+final places =
+    GoogleMapsPlaces(apiKey: "AIzaSyAsVrHxXGITDH-g6ozMNeoEAtiFuhGulwI");
+
+class _FoodieMapState extends State<FoodieMap> {
+  Future<Position>? _currentLocation;
+  Set<Marker> _markers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    _currentLocation = Geolocator.getCurrentPosition();
+  }
+
+  Future<void> _retrieveNearbyRestaurants(LatLng _userLocation) async {
+    PlacesSearchResponse _response = await places.searchNearbyWithRadius(
+        Location(lat: _userLocation.latitude, lng: _userLocation.longitude),
+        10000,
+        type: "hospital");
+
+    Set<Marker> _restaurantMarkers = _response.results
+        .map((result) => Marker(
+            markerId: MarkerId(result.name),
+            // Use an icon with different colors to differentiate between current location
+            // and the restaurants
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueAzure),
+            infoWindow: InfoWindow(
+                title: result.name,
+                snippet:
+                    "Ratings: " + (result.rating?.toString() ?? "Not Rated")),
+            position: LatLng(
+                result.geometry!.location.lat, result.geometry!.location.lng)))
+        .toSet();
+
+    setState(() {
+      _markers.addAll(_restaurantMarkers);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _currentLocation,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasData) {
+              // The user location returned from the snapshot
+              Position snapshotData = snapshot.data as Position;
+              LatLng _userLocation =
+                  LatLng(snapshotData.latitude, snapshotData.longitude);
+
+              if (_markers.isEmpty) {
+                _retrieveNearbyRestaurants(_userLocation);
+              }
+
+              return GoogleMap(
+                initialCameraPosition: CameraPosition(
+                  target: _userLocation,
+                  zoom: 12,
+                ),
+                markers: _markers
+                  ..add(Marker(
+                      markerId: MarkerId("User Location"),
+                      infoWindow: InfoWindow(title: "User Location"),
+                      position: _userLocation)),
+              );
+            } else {
+              return Center(child: Text("Failed to get user location."));
+            }
+          }
+          // While the connection is not in the done state yet
+          return Center(child: CircularProgressIndicator());
+        });
+  }
+}
